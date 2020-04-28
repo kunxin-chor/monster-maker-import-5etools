70cd821536ef95e68f6b5f666184e25f56d71750
kunxin-chor <chorkunxin@yahoo.com>
2020-04-28 18:37:44 +0000

    Added converted damage toll

--- parse-monster-maker-html.js
+++ parse-monster-maker-html.js
@@ -101,6 +101,132 @@ function getListings($m, listingClass) {
     return $m.find(`.${listingClass} span`).eq(2).text().trim();
 }
 
+function parseAllActions(actionString) {
+    let actions = $('.monster-action > p').map(function(action){
+        $a = $(action);
+        return {
+            name: $a.find('span').eq(0).text(),
+            entry: parseAction( $a.find('span').eq(1).text())
+        }
+    })
+}
+
+function parseAction(actionString) {
+
+}
+
+function getAttacks(actionString) {
+    let attacks = [];
+    let words = actionString.split(' ');
+    for (let i=0; i < words.length; i++) {
+        let w = words[i];
+        if (w=='vs' || w=="vs.") {
+            let a = {
+                original: `${words[i-1]} ${words[i]} ${words[i+1]}`,
+                replace_by: convertAttack(words[i-1], words[i+1])
+            }
+            attacks.push(a);
+        }
+        
+    }
+    return attacks;
+}
+
+function convertAttack(value, attackType) {
+
+    // remove puntuctation
+    let attackTypeToMatch = attackType.replace('.', '').replace(',', '').trim();
+
+    let converted="";
+    if (attackTypeToMatch.toLowerCase()=="ac") {
+        converted = `@{hit ${value}} to hit, `
+    } else {
+        converted = `Make a @{dc ${value}} ${attackType} saving throw,`
+    }
+    return converted;
+
+    
+
+
+}
+
+
+function getDamageComponents(actionString) {
+    // pattern to match:
+    // X damage
+    // X <damage type> damage
+    // X (<dice notation>) damage
+    // X (<dice notation>) <damage type> damage
+ 
+
+   // find all occurrences of damage
+   let results = [];
+   let lastFoundIndex = actionString.indexOf('damage');
+   let breaker2 = 0;
+   while (lastFoundIndex != -1 && lastFoundIndex < actionString.length && breaker2 < 1000) {
+       breaker2++;
+        if (breaker2==100) {
+            break;
+        }
+        // trace backward till we find a number not enclosed within a parethensis
+        let haveParenthesis = false;
+        let foundMatchingParenthesis = false;
+        let i = lastFoundIndex;
+        let buffer = [];
+        let startChange = 0;
+        let endChange = lastFoundIndex;
+        let breaker=0;
+
+        while (i>0) {
+            breaker++;
+            if (breaker > 100) {
+                break;
+            }
+            if (actionString[i]==')') {
+                haveParenthesis = true;
+            } else if (actionString[i]=='(') {
+                foundMatchingParenthesis = true;
+            } else if (actionString[i] != " " && !isNaN(actionString[i]) && (haveParenthesis==false || (haveParenthesis && foundMatchingParenthesis))) {
+                buffer.push(actionString[i])
+            } else if (actionString[i]==' ' && buffer.length > 0) {
+                console.log(buffer)
+                startChange = i + 1;
+                let damage = parseInt(buffer.reduce(function(current, fused){
+                    return fused+=current;
+                }));
+
+                let damageType = getDamageType(actionString.substring(startChange, endChange));
+
+                results.push({
+                    start: startChange,
+                    end: endChange,
+                    changeTo: `${convertDamage(damage)} ${damageType} damage` 
+                })
+                break;
+
+            }
+            i--;
+        }
+        lastFoundIndex = actionString.substring(lastFoundIndex+"damage".length, actionString.length-1).indexOf('damage');
+        console.log("next last found index="+lastFoundIndex);
+   }
+   return results;
+
+}
+
+function getDamageType(damageString) {
+    let chunks = damageString.split(' ');
+    return chunks[ chunks.length- 2];
+}
+
+function convertDamage(damage) {
+   return `{@h}${damage} ({%damage ${getDamageDice(damage)}d6})`;
+}
+
+function getDamageDice(damage) {
+    return Math.ceil(damage*2/6);
+}
+
 function parseHTMLStatBlock(html) {
     let $m= $(html);
 
@@ -125,14 +251,17 @@ function parseHTMLStatBlock(html) {
         }
     ]
 
+    // MONSTER HP
     let hp = parseInt($m.find('.monster-hp span').eq(1).text.trim());
     monster.hp = {
         'average': hp, //"Hit Points X"
         'formula': Math.ceil(hp/6)
     }
 
+    // MONSTER SPEED
     monster.speed = parseSpeed($m.find('.monster-speed span').eq(1).text.trim());
 
+    // MONSTER SCORES
     let abilities = $m.find('.monster-ability');
     let scores = [];
     for (let a of abilities) {
@@ -173,8 +302,33 @@ function parseHTMLStatBlock(html) {
         return s.trim();
     });
 
+    // LANGUAGES
     monster.languages = getListings($m, 'monster-languages').split(', ').map(function(s){
         return s.trim();
+    })
+
+    monster.cr = getListings($m, 'monster-challenge').text().trim();
+
+    monster.vulnerable = getListings($m, 'monster-vulnerabilities').text().trim();
+
+    monster.traits = $m.find('.monster-trait').map(function(t){
+       let spans = $(t).find('span');
+       return {
+           'name': spans[0].innerText,
+           'entries': [
+               spans[1].innerText
+           ]
+       }
+    })
+
+    monster.actions = $m.find('monster-action').map(function(t){
+       let spans = $(t).find('span');
+       return {
+           'name': spans[0].innerText,
+           'entries': [
+               spans[1].innerText
+           ]
+       }
     })
 
 }
